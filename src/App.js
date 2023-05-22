c7d67240fa7a5100b90d274f0c52c675803b90b6
deena-neirat <deena.nairat2017@gmail.com>
2023-05-22 15:48:00 +0300

    task2

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import About from './components/About/About';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <>
+    <About />
+    <Home />
+    </>
+  );
+}
+
+export default App;
